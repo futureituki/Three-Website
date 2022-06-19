fd4c80a0fe7f81313ad4e14a5f5c542d5ae636e2
CreateWebSiteTeam <yourgitemail@example.com>
2022-06-20 07:29:21 +0900

    eslint

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const withTM = require('next-transpile-modules')(['@react-three/drei', 'three'])
 const config = withTM()
-
+const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = Object.assign(config, nextConfig)
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  }
+};
