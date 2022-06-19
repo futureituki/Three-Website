ad77c60d60dcf5e24a5aeb43af5ce7a280b6a248
CreateWebSiteTeam <yourgitemail@example.com>
2022-06-20 07:51:33 +0900

    style

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 
 import { Canvas } from "@react-three/fiber"
-import NavBar from "../component/navbar"
 import Header from "../component/navbar/nav"
 // import '../styles/styles.css'
-function MyApp({ Component, pageProps, router }) {
+function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <NavBar router={router}/> */}
       <Header/>
       <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
       <Component {...pageProps} />
